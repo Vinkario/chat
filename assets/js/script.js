94d5b98c8827f06b88c8db265001c340c1181f72
rogerio bueno <rogerio.bueno@globalsys.com.br>
2023-01-10 16:23:32 -0300

    feat: html and css

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const conversas = document.getElementById('chat-text')
+const input = document.getElementById("text-input")
+const button = document.getElementsByTagName('button')
+
+
+function enviar (){
+    
+
+
+}
+
