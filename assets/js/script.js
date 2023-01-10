7965a1c9b95e364b7f63a9a3101185321fe2b72a
rogerio bueno <rogerio.bueno@globalsys.com.br>
2023-01-10 17:24:09 -0300

    perai que ta uma bagunca

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
-const conversas = document.getElementById('chat-text')
-const input = document.getElementById("text-input")
-const button = document.getElementsByTagName('button')
+const createElement = (chat, sendBy) => {
+const messageBox = document.getElementById("message") 
+const pElement = document.createElement("p")
 
+pElement.innerHTML = chat
+pElement.style.color = (sendBy == "2") && "red"
 
-function enviar (){
-    
-
-
+messageBox.appendChild(pElement)
 }
 
+const init = () => {
+const button = document.getElementById("enviar")
+const input = document.getElementById("message-input")
+const select = document.getElementById("escolha")
+
+button.onclick = () => {
+const message = input.value
+const sendBy = select.value
+
+createElement(message, sendBy)
+
+input.value = ""
+}
+}
+
+
+init()
